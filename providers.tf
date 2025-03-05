832813b06f344ee62b7983143703cd8ac758c645
mervin.hemaraju <mervinhemaraju16@gmail.com>
2025-03-05 15:50:49 +0400

    - Create a default management project and attached billing account to it

--- providers.tf
+++ providers.tf
@@ -22,6 +22,14 @@ terraform {
   }
 }
 
+# Configure the provider not to use the specified project for quota check.
+# This provider should only be used during project creation and initializing services.
+provider "google" {
+  alias                 = "no_user_project_override"
+  user_project_override = false
+}
+
+# Configure the provider that uses the new project's quota.
 provider "google" {
   user_project_override = true
 }
