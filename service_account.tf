36d18b26e94c97e304811951218c7fd9bcdd2857
mervin.hemaraju <mervinhemaraju16@gmail.com>
2025-03-06 00:46:53 +0400

    - Added org admin role to the github service account

--- service_account.tf
+++ service_account.tf
@@ -36,6 +36,13 @@ resource "google_project_iam_member" "github_sa_firebase_admin" {
   member  = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/mervinhemaraju/glamu-iac"
 }
 
+# Add Organization Administrator role
+resource "google_organization_iam_member" "github_sa_org_admin" {
+  org_id = data.google_organization.mervinhemaraju.org_id
+  role   = "roles/resourcemanager.organizationAdmin"
+  member = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/mervinhemaraju/glamu-iac"
+}
+
 # Create a service account IAM policy binding
 resource "google_service_account_iam_binding" "github_workload_identity_binding" {
   service_account_id = google_service_account.github.name
