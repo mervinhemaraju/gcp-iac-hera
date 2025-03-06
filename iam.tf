30a01d9f53ceeeb9afeacf84d4f6da74ae8d6c0b
mervin.hemaraju <mervinhemaraju16@gmail.com>
2025-03-06 10:04:05 +0400

    -

--- iam.tf
+++ iam.tf
@@ -1,10 +1,4 @@
-resource "google_organization_iam_member" "project_creator" {
-  org_id = data.google_organization.mervinhemaraju.org_id
-  role   = "roles/resourcemanager.projectCreator"
-  member = "serviceAccount:${google_service_account.github.email}"
-}
 
-// Existing Project Creator role
 resource "google_organization_iam_member" "project_creator" {
   org_id = data.google_organization.mervinhemaraju.org_id
   role   = "roles/resourcemanager.projectCreator"
